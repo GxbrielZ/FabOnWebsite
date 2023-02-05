9654fdce63d6de9bd02fa234aa4ac44d74ece3d1
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-02-05 19:02:16 +0100

    Added video as home background. Added some code fixes.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
+const navigation = document.querySelector(".nav");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
